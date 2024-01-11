3f54b7db6b0188ae1ec441803adb3275e355c19f
3BeBeBe <ptbgs.adhipradana84@gmail.com>
2024-01-11 10:04:02 +0700

    AutomaticCarousal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
   <div class="home">
     <NavBar/>
     <CarousalInfo/>
+    <Ctry/>
     <FlashInfo/>
     <ItemCard/>
     <FlashNews/>
     <FooterSection/>
+    
   </div>
 </template>
 
@@ -17,6 +19,7 @@ import NavBar from '@/components/NavBar.vue'
 import FlashInfo from '@/components/FlashInfo.vue'
 import FlashNews from '@/components/FlashNews.vue'
 import FooterSection from '@/components/FooterSection.vue'
+import Ctry from '@/components/1_try.vue'
 
 export default {
   name: 'HomeView',
@@ -26,7 +29,8 @@ export default {
     ItemCard,
     FlashInfo,
     FlashNews,
-    FooterSection
+    FooterSection,
+    Ctry
   }
 }
 </script>
