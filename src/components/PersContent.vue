56327325956304ad2751b01cbf326054bf73ad73
andimaar-264 <maarsh.2112@gmail.com>
2024-01-15 17:09:26 +0700

    Update PersContent.vue

--- src/components/PersContent.vue
+++ src/components/PersContent.vue
@@ -1,78 +1,72 @@
 <template>
-  <div class="container mt-5">
-      <div class="row no-gutters">
+  <div class="card">
+      <div class="card-header text-center">
+          <ul class="nav nav-underline justify-content-center">
+          <li class="nav-item">
+              <h3><a id="tab1" class="nav-link active" data-bs-toggle="tab" aria-current="true" href="#artikel1">Artikel</a></h3>
+          </li>
+          <li class="nav-item">
+              <h3><a id="tab1" class="nav-link" data-bs-toggle="tab" aria-current="true" href="#pers1">Siaran Pers</a></h3>            
+          </li>
+          </ul>
+      </div>
+      
+      </div>
+      <div class="container tab-content mt-2">
+      <div class="tab-pane fade show active" id="artikel1">
+      <div class="row mt-4 ">
           <div class="col-md-6">
-              <div class="overlay-container">
-                  <img src="../assets/Galeri/Kunjungan ke PT RDM/1.jpeg" class="img-fluid">
-                  <div class="overlay-text">
-                      <h2><b>Kunjungan ke PT RDM </b></h2>
-                  </div>
-              </div>
+              <img class="img-fluid" src="https://www.pi-utilitas.com/files/media/2022/11/WhatsApp-Image-2022-11-18-at-11.19.02.jpeg">
+
+              <h4>Berdayakan Kelompok Wanita, PI Utilitas Bentuk Bank Sampah Desa Banyuwangi</h4>
+
+              <p>Makna dari pepatah inilah yang menjadi semangat PI Utilitas dalam menjalankan program Corporate Social Responsibility (CSR).</p>
           </div>
           <div class="col-md-6">
-              <div class="row no-gutters">
-                  <div class="col-md-12 bg-primary">
-                  <div class="overlay-container">
-                      <img src="../assets/Galeri/Sedekah Hewan Qurban/1.jpeg" class="img-fluid">
-                      <div class="overlay-text">
-                          <h3><b>Sedekah Hewan Qurban</b></h3>
-                      </div>
-                  </div>
-              </div>
-                  <div class="col-md-6">
-                      <div class="overlay-container">
-                          <img src="../assets/Galeri/Donor Darah Sewindu PI Utilitas/1.jpg" class="img-fluid">
-                          <div class="overlay-text">
-                              <h3><b>Donor Darah Sewindu PI Utilitas</b></h3>
-                          </div>
-                      </div>
-                  </div>
-              <div class="col-md-6">
-                  <div class="overlay-container">
-                      <img src="../assets/Galeri/Penanaman Mangrove/1.jpg" class="img-fluid">
-                      <div class="overlay-text">
-                          <h3><b>Penanaman Mangrove</b></h3>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-md-6">
-                  <div class="overlay-container">
-                      <img src="../assets/Galeri/Pelatihan Manajemen Destinasi Wisata/1.jpeg" class="img-fluid">
-                      <div class="overlay-text">
-                          <h3><b>Pelatihan Manajemen Destinasi Wisata</b></h3>
-                      </div>
-                  </div>
-              </div>
+              <img class="img-fluid" src="https://www.pi-utilitas.com/files/media/2022/09/TOP-GRC-AWARDS.jpg">
+
+              <h4>Pupuk Indonesia Utilitas Raih Tiga Penghargaan di Ajang TOP GRC Awards 2022 </h4>
               
-              </div>
+              <p>Pupuk Indonesia Utilitas (PI Utilitas), salah satu anak perusahaan Pupuk Indonesia (Persero) berhasil meraih tiga penghargaan TOP Governance, Risk and Compilance (Tata Kelola, Risiko dan Kepatuhan) Awards 2022 yang digelar di Hotel Raffles, Jakarta, Selasa, 6 September 2022.</p>
           </div>
+          <div class="col-md-6">
+              <img class="img-fluid" src="https://www.pi-utilitas.com/files/media/2022/10/buah-nipah.jpg">
+
+              <h4>Terus Kembangkan Potensi, PI Utilitas Bekali Warga Cara Mengolah Buah Nipah </h4>
+              
+              <p> PI Utilitas terus mengembangkan potensi Desa Wisata binaan yang menjadi program CSR PI Utilitas yaitu Desa Wisata Terintegrasi dan Berkelanjutan “Desibel” Pecuk Indah Ecoriparian melalui optimalisasi potensi hasil alam yang ada di wilayah desa wisata berupa buah nipah yang diwujudkan dalam program “Pelatihan Cara Mengolah Buah Nipah” yang dilaksanakan pada tanggal 12 dan 13 Oktober 2022.</p>
+          </div>
+      </div>
+      </div>
+      <div class="tab-pane fade" id="pers1">
+      <div class="row mt-4 ">
+          <div class="col-md-6">
+              <img class="img-fluid" src="https://www.pi-utilitas.com/files/media/2022/11/WhatsApp-Image-2022-11-18-at-11.19.02.jpeg">
+
+              <h4>Berdayakan Kelompok Wanita, PI Utilitas Bentuk Bank Sampah Desa Banyuwangi</h4>
+
+              <p>Makna dari pepatah inilah yang menjadi semangat PI Utilitas dalam menjalankan program Corporate Social Responsibility (CSR).</p>
+          </div>
+          <div class="col-md-6">
+              <img class="img-fluid" src="https://www.pi-utilitas.com/files/media/2022/09/TOP-GRC-AWARDS.jpg">
+
+              <h4>Pupuk Indonesia Utilitas Raih Tiga Penghargaan di Ajang TOP GRC Awards 2022 </h4>
+              
+              <p>Pupuk Indonesia Utilitas (PI Utilitas), salah satu anak perusahaan Pupuk Indonesia (Persero) berhasil meraih tiga penghargaan TOP Governance, Risk and Compilance (Tata Kelola, Risiko dan Kepatuhan) Awards 2022 yang digelar di Hotel Raffles, Jakarta, Selasa, 6 September 2022.</p>
+          </div>
+          <div class="col-md-6">
+              <img class="img-fluid" src="https://www.pi-utilitas.com/files/media/2022/10/buah-nipah.jpg">
+
+              <h4>Terus Kembangkan Potensi, PI Utilitas Bekali Warga Cara Mengolah Buah Nipah </h4>
+              
+              <p> PI Utilitas terus mengembangkan potensi Desa Wisata binaan yang menjadi program CSR PI Utilitas yaitu Desa Wisata Terintegrasi dan Berkelanjutan “Desibel” Pecuk Indah Ecoriparian melalui optimalisasi potensi hasil alam yang ada di wilayah desa wisata berupa buah nipah yang diwujudkan dalam program “Pelatihan Cara Mengolah Buah Nipah” yang dilaksanakan pada tanggal 12 dan 13 Oktober 2022.</p>
+          </div>
+      </div>
       </div>
   </div>
 </template>
 <script>
   export default {
-      name: 'PersContent'
+      name: 'NewsContents'
   }
 </script>
-<style>
-  .responsive-image {
-    width: 100%; /* Set the width to 100% to make the image responsive */
-    height: 50%; /* Maintain the aspect ratio */
-  }
-
-  .overlay-text {
-    position: absolute;
-    top: 80%;
-    left: 5%; /* Adjust this value to move the text to the left */
-    transform: translate(0%, -70%);
-    color: white;
-    font-size: 20px;
-    font-weight: bold;
-  }
-
-  .overlay-container {
-    position: relative;
-    width: 100%;
-    height: 100%;
-  }
-</style>
