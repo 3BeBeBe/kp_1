02117222bc9870d9f381c628a685bbf5f83f4282
3BeBeBe <ptbgs.adhipradana84@gmail.com>
2024-01-17 12:19:04 +0700

    Update

--- src/components/AuditCharter.vue
+++ src/components/AuditCharter.vue
@@ -9,7 +9,7 @@
                     <p>Piagam Satuan Pengawas Intern merupakan dokumen formal yang menjelaskan struktur dan kedudukan Unit Satuan Pengawas Intern, kualifikasi, ruang lingkup kerja operasional, wewenang, tanggungjawab, standar professional, hubungan dengan auditor eksternal dan Komite Audit, kode etik, serta independensi dan obyektivitas SPI. Piagam Satuan Pengawas Intern menjadi dasar bagi SPI dalam melaksanakan tugasnya serta sifatnya mengikat kepada seluruh unit kerja dan anak perusahaan.</p>
                 </div>
                 <div>
-                    <a href="../assets/tataKelola/PIAGAM-SATUAN-PENGAWASAN-INTERN-INTERNAL-AUDIT-CHARTER.pdf" class="btn btn-primary">Audit Charter</a>
+                    <a href="../assets/tataKelola/PIAGAM-SATUAN-PENGAWASAN-INTERN-INTERNAL-AUDIT-CHARTER.pdf" class="btn btn-primary" download>Audit Charter</a>
                 </div>
             </div>
         </div>
--- src/components/SOPTable.vue
+++ src/components/SOPTable.vue
@@ -1,43 +1,29 @@
 <template>
     <div class="container mt-5 mb-5">
-        <h3  class="text-center">MANUAL</h3>
-        <br>
         <div class="row justify-content-center">
-        <div class="col-md-3 ">
-          
+        <div class="col-md-3 d-flex flex-column justify-content-between">
                 <h5 >Pedoman GCG</h5>
-                <br>
                 <div class="text-end">
-                    <a href="#" class="btn btn-primary">Read More</a>
+                    <a href="../assets/tataKelola/PEDOMAN-PENILAIAN-PENERAPAN-GCG-GCG-ASSESSMENT.pdf" class="btn btn-primary" download>Download</a>
                 </div>
-                <br>
         </div>
-        <div class="col-md-3 ">
-      
+        <div class="col-md-3 d-flex flex-column justify-content-between">
                 <h5 >Board Manual</h5>
-                <br>
                 <div class="text-end">
-                    <a href="#" class="btn btn-primary">Read More</a>
+                    <a href="../assets/tataKelola/Board-Manual_compressed.pdf" class="btn btn-primary" download>Download</a>
                 </div>
-                <br>
         </div>
-        <div class="col-md-3 ">
-            
+        <div class="col-md-3 d-flex flex-column justify-content-between">
                 <h5 >Pedoman COC</h5>
-                <br>
                 <div class="text-end">
-                    <a href="#" class="btn btn-primary">Read More</a>
+                    <a href="../assets/tataKelola/PEDOMAN-ETIKA-DAN-PERILAKU-CODE-OF-CONDUCT.pdf" class="btn btn-primary" download>Download</a>
                 </div>
-            <br>
         </div>
-        <div class="col-md-3 ">
-            
+        <div class="col-md-3 d-flex flex-column justify-content-between">
                 <h5 >Pedoman Pengendalian Gratifikasi</h5>
-                 
                 <div class="text-end">
-                    <a href="#" class="btn btn-primary">Read More</a>
+                    <a href="../assets/tataKelola/gratifikasi.pdf" class="btn btn-primary" download>Download</a>
                 </div>
-            
         </div>
         
         </div>
