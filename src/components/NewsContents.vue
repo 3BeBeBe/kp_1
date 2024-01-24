6339329f970cfc5d67385bcbb5c9a56992de3119
andimaar-264 <maarsh.2112@gmail.com>
2024-01-24 11:52:03 +0700

    artikel changing

--- src/components/NewsContents.vue
+++ src/components/NewsContents.vue
@@ -227,17 +227,211 @@
                         <div class="row">
                             <div class="col-md-4 mb-3">
                                 <button class="card card-fixed">
-                                    <img src="https://www.pi-utilitas.com/files/media/2022/09/TOP-GRC-AWARDS.jpg" class="card-img-top img-fixed" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title text-start">Pupuk Indonesia Utilitas Raih Tiga Penghargaan di Ajang TOP GRC Awards 2022 </h5>
+                                    <img src="../assets/artikel/Artikel_1.jpeg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Berdayakan Kelompok Wanita, PI Utilitas Bentuk Bank Sampah Desa Banyuwangi</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">11 November 2022</small></p>
                                     </div>
                                 </button>
                             </div>
                             <div class="col-md-4 mb-3">
                                 <button class="card card-fixed">
-                                    <img src="https://www.pi-utilitas.com/files/media/2022/09/TOP-GRC-AWARDS.jpg" class="card-img-top img-fixed" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title text-start">Pupuk Indonesia Utilitas Raih Tiga Penghargaan di Ajang TOP GRC Awards 2022 </h5>
+                                    <img src="../assets/artikel/Artikel_2.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title"> Terus Kembangkan Potensi, PI Utilitas Bekali Warga Cara Mengolah Buah Nipah</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">28 Oktober 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_3.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Komisi VII DPR RI Lakukan Kunjungan Kerja Spesifik ke GGCP</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">17 September 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <!-- <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/siaranPers/SP-16.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">PI Utilitas Terima Kunjungan Kerja Spesifik</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">17 September 2022</small></p>
+                                    </div>
+                                </button>
+                            </div> -->
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_4.jpeg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title"> PI Utilitas Disiplin Implementasikan GRC</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">31 Agustus 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_5.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Dukungan Keuangan untuk Pengembangan Bisnis Utilitas Berbasis Energi Baru Terbarukan</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">24 Agustus 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_6.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Bisnis Utilitas Berbasis Energi Baru Terbarukan</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">12 Agustus 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_7.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Masa Depan Bisnis Utilitas</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">1 Agustus 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_8.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">PT Pupuk Indonesia Utilitas Catatkan Kinerja Positif Selama Tahun 2021</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">11 Juli 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_9.png" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">e-Announcement LHKPN Pupuk Indonesia Group</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 Januari 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_10.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Dua Sertifikat ISO Diraih Sekaligus Oleh PT Pupuk Indonesia Utilitas</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">8 Januari 2022</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_11.png" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Kebijakan Mutu, Keselamatan dan Kesehatan Kerja, Lindung Lingkungan dan Anti Penyuapan</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">2 April 2021</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_12.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Tingkatkan Kompetensi SDM, PI Utilitas Tandatangani MoU dengan UGM</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">9 Februari 2021</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_13.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Kunjungi Yogyakarta, Dirut PI-Energi Dalami Potensi Energi Baru Terbarukan</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 Februari 2020</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/artikel/Artikel_14.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Lestarikan Ekowisata, PT Pupuk Indonesia Energi Salurkan Dana CSR</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">17 Februari 2020</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/siaranPers/SP-16" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Jelajah</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 November 2019</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/siaranPers/SP-16.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Tiga Masalah, Tiga Solusi</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 November 2019</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/siaranPers/SP-16.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Tumbuh Kembang Energi Listrik Di Indonesia</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 November 2019</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/siaranPers/SP-16.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title"> Prospek Energi Surya Dan Mini Hidro Di Indonesia</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 November 2019</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/siaranPers/SP-16.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title"> Efisiensi Yang Telah Teruji</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 November 2019</small></p>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <button class="card card-fixed">
+                                    <img src="../assets/siaranPers/SP-16.jpg" class="card-img-top img-fixed">
+                                    <div class="card-body text-start">
+                                        <h5 class="card-title">Pupuk Indonesia Energi, Hadir Untuk Efisiensi</h5>
+
+                                        <p class="card-text text-start"><small class="text-body-secondary">29 November 2019</small></p>
                                     </div>
                                 </button>
                             </div>
