41fee3f35c171dd195800eb691a4adc272a1e8ea
andimaar-264 <maarsh.2112@gmail.com>
2024-02-06 11:23:01 +0700

    lots of changes

--- src/components/CompanyHistory.vue
+++ src/components/CompanyHistory.vue
@@ -1,38 +1,72 @@
 <template>
-    <div class="company-history-container">
-      <div class="container">
-        <div class="background-overlay"></div>
-        <div class="background-image"></div>
-        <div class="row justify-content-center">
-          <div class="col-md-12 mt-5 fade-in-right" ref="fadeInElement3">
-            <h2><i><b>Sejarah Perusahaan</b></i></h2>
-            <br>
+  <div class="company-history-container">
+    <div class="container">
+      <div class="background-overlay"></div>
+      <div class="background-image"></div>
+      <div class="row justify-content-center">
+        <div class="col-md-12 mt-5 fade-in-right" ref="fadeInElement3">
+          <h2>
+            <i><b>Sejarah Perusahaan</b></i>
+          </h2>
+          <br />
+        </div>
+        <div class="col-md-6 pr-md-5 fade-in-right" ref="fadeInElement1">
+          <img src="../assets/pabrik/GGCP/ggcp4.jpg" class="img-fluid" />
+
+          <p class="text-justify">
+            Perusahaan yang bergerak dalam bidang industri penghasil energi dan
+            utilitas untuk seluruh Anak Perusahaan yang bernaung di dalam
+            kelompok usaha PT Pupuk Indonesia (Persero) yang bergerak di bidang
+            industri pupuk dan agrokimia.
+          </p>
+          <p class="text-justify">
+            Pendirian Perusahaan dimaksudkan sebagai langkah untuk meningkatkan
+            daya saing BUMN Pupuk dan mengupayakan efisiensi biaya produksi
+            pupuk oleh BUMN Pupuk melalui usaha penggunaan teknologi yang ramah
+            lingkungan dan melakukan penghematan bahan baku dan energi.
+          </p>
+        </div>
+
+        <div class="col-md-6 pl-mb-5 fade-in-left" ref="fadeInElement2">
+          <div class="mb-4">
+            <br />
+            <p class="text-justify">
+              Dengan kecenderungan produksi Gas Bumi Nasional yang menurun serta
+              tren harga gas yang terus meningkat, dan untuk menjalankan amanat
+              Instruksi Presiden No. 2 Tahun 2010 tentang revitalisasi industri
+              pupuk guna tetap menjaga keberlangsungan usaha dan daya saing
+              Perusahaan Pupuk di grup PIHC, PT Pupuk Indoensia (Persero)
+              bersama-sama dengan Anak Perusahaannya yaitu PT Petrokima Gresik,
+              PT Pupuk Kujang Cikampek, PT Pupuk Kalimantan Timur, PT Pupuk
+              Iskandar Muda, PT Pupuk Sriwidjaja Palembang dan Rekayasa Industri
+              mendirikan PI Utilitas untuk memproduksi dan mensuplai energi dan
+              utilitas ke Perusahaan-Perusahaan di lingkungan kelompok usaha PT
+              Pupuk Indonesia (Persero) pada khususnya, serta ke industri atau
+              Perusahaan lainnya pada umumnya.
+            </p>
           </div>
-          <div class="col-md-6 pr-md-5 fade-in-right mb-5" ref="fadeInElement1">
-            
-            <img src="../assets/pabrik/GGCP/ggcp4.jpg" class="img-fluid">
-            
-            <p class="text-justify">Perusahaan yang bergerak dalam bidang industri penghasil energi dan utilitas untuk seluruh Anak Perusahaan yang bernaung di dalam kelompok usaha PT Pupuk Indonesia (Persero) yang bergerak di bidang industri pupuk dan agrokimia.</p>
-            <p class="text-justify">Pendirian Perusahaan dimaksudkan sebagai langkah untuk meningkatkan daya saing BUMN Pupuk dan mengupayakan efisiensi biaya produksi pupuk oleh BUMN Pupuk melalui usaha penggunaan teknologi yang ramah lingkungan dan melakukan penghematan bahan baku dan energi.</p>
-          </div>
-  
-          <div class="col-md-6 pl-mb-5 fade-in-left" ref="fadeInElement2">
-            <div class="mb-4">
-              <br>
-              <p class="text-justify">Dengan kecenderungan produksi Gas Bumi Nasional yang menurun serta tren harga gas yang terus meningkat, dan untuk menjalankan amanat Instruksi Presiden No. 2 Tahun 2010 tentang revitalisasi industri pupuk guna tetap menjaga keberlangsungan usaha dan daya saing Perusahaan Pupuk di grup PIHC, PT Pupuk Indoensia (Persero) bersama-sama dengan Anak Perusahaannya yaitu PT Petrokima Gresik, PT Pupuk Kujang Cikampek, PT Pupuk Kalimantan Timur, PT Pupuk Iskandar Muda, PT Pupuk Sriwidjaja Palembang dan Rekayasa Industri mendirikan PI Utilitas untuk memproduksi dan mensuplai energi dan utilitas ke Perusahaan-Perusahaan di lingkungan kelompok usaha PT Pupuk Indonesia (Persero) pada khususnya, serta ke industri atau Perusahaan lainnya pada umumnya.</p>
+          <div>
+            <div class="rounded-image">
+              <!-- <img src="https://www.pi-energi.com/files/media/2020/09/image-banner-factory.jpg" class="img-fluid"> -->
             </div>
-            <div>
-              <div class="rounded-image">
-                <!-- <img src="https://www.pi-energi.com/files/media/2020/09/image-banner-factory.jpg" class="img-fluid"> -->
-              </div>
-              <p class="text-justify">Tanggal 1 Desember 2021 PT Pupuk Indonesia Utilitas melakukan rebranding dari yang sebelumnya bernama PT Pupuk Indonesia Energi sesuai dengan perubahan Anggaran Dasar PT Pupuk Indonesia Utilitas yang terakhir dimuat dalam Akta No. 02 Tanggal 28 Oktober 2021. Rebranding ini mengukuhkan PI Utilitas sebagai one stop utility provider untuk menunjang tiga komponen kebutuhan utama pabrik pupuk meliputi utilitas, amoniak, dan urea. Utilitas berupa listrik, steam, air, dan nitrogen membuat ruang lingkup bisnis perusahaan lebih luas dan potensi pasar juga lebih besar.</p>
+            <p class="text-justify">
+              Tanggal 1 Desember 2021 PT Pupuk Indonesia Utilitas melakukan
+              rebranding dari yang sebelumnya bernama PT Pupuk Indonesia Energi
+              sesuai dengan perubahan Anggaran Dasar PT Pupuk Indonesia Utilitas
+              yang terakhir dimuat dalam Akta No. 02 Tanggal 28 Oktober 2021.
+              Rebranding ini mengukuhkan PI Utilitas sebagai one stop utility
+              provider untuk menunjang tiga komponen kebutuhan utama pabrik
+              pupuk meliputi utilitas, amoniak, dan urea. Utilitas berupa
+              listrik, steam, air, dan nitrogen membuat ruang lingkup bisnis
+              perusahaan lebih luas dan potensi pasar juga lebih besar.
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  </div>
 </template>
-  <script>
+<script>
 export default {
   name: "CompanyHistory",
   data() {
@@ -40,23 +74,52 @@ export default {
       lastScrollTop: 0,
     };
   },
-  mounted() {
-    // Listen for scroll events
-    window.addEventListener("scroll", this.handleScroll);
+  // mounted() {
+  //   // Listen for scroll events
+  //   window.addEventListener("scroll", this.handleScroll);
 
-    // Initial observation on mount
+  //   // Initial observation on mount
+  //   this.handleScroll();
+  // },
+  // unmounted() {
+  //   // Clean up event listener when the component is unmounted
+  //   window.removeEventListener("scroll", this.handleScroll);
+  // },
+
+  // mounted() {
+  //   // Listen for scroll events
+  //   window.addEventListener("scroll", this.handleScroll);
+
+  //   // Listen for resize events
+  //   window.addEventListener("resize", this.handleScroll);
+
+  //   // Initial observation on mount
+  //   this.handleScroll();
+  // },
+
+  mounted() {
+  // Listen for scroll events
+  window.addEventListener("scroll", this.handleScroll);
+
+  // Listen for resize events
+  window.addEventListener("resize", this.handleScroll);
+
+  // Initial observation on mount
+  this.$nextTick(() => {
     this.handleScroll();
-  },
+  });
+},
   unmounted() {
-    // Clean up event listener when the component is unmounted
+    // Clean up event listeners when the component is unmounted
     window.removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener("resize", this.handleScroll);
   },
   methods: {
     handleScroll() {
       const fadeInElements = [
         this.$refs.fadeInElement1,
         this.$refs.fadeInElement2,
-        this.$refs.fadeInElement3
+        this.$refs.fadeInElement3,
       ].filter(Boolean);
 
       fadeInElements.forEach((element) => {
@@ -77,12 +140,12 @@ export default {
 };
 </script>
 
-  <style scoped>
-  .company-history {
-    margin-top: 100px;
-  }
-  
-  .company-history-container {
+<style scoped>
+.company-history {
+  margin-top: 100px;
+}
+
+.company-history-container {
   position: relative;
   color: white; /* Set text color to contrast with the background */
 }
@@ -93,7 +156,12 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(8, 78, 139, 0.8); /* Use rgba for the background color with alpha channel */
+  background-color: rgba(
+    8,
+    78,
+    139,
+    0.8
+  ); /* Use rgba for the background color with alpha channel */
   content: "";
   z-index: -1;
   opacity: 0.9;
@@ -129,19 +197,19 @@ export default {
   bottom: 0;
   background: linear-gradient(rgba(8, 78, 139, 0), rgba(8, 78, 139, 0.8));
 }
-  
-  .company-history img {
-    width: 100%;
-    height: 50%;
-    opacity: 0.5;
-  }
-  
-  .rounded-image {
-    border-radius: 25px; /* Adjust the border-radius to control the roundness of the corners */
-    overflow: hidden;
-  }
-  
-  .fade-in-left {
+
+.company-history img {
+  width: 100%;
+  height: 50%;
+  opacity: 0.5;
+}
+
+.rounded-image {
+  border-radius: 25px; /* Adjust the border-radius to control the roundness of the corners */
+  overflow: hidden;
+}
+
+.fade-in-left {
   opacity: 0;
   transform: translateX(20px); /* Adjust this value for the desired direction */
   transition: opacity 0.8s ease, transform 0.8s ease;
@@ -166,8 +234,14 @@ export default {
   transition: opacity 0.5s ease, transform 0.5s ease;
 }
 
-.fade-in.in-viewport{
-    opacity: 1;
-    transform: translateX(0);
+.fade-in.in-viewport {
+  opacity: 1;
+  transform: translateX(0);
 }
+
+.in-viewport {
+  opacity: 1;
+  transform: translateX(0);
+}
+
 </style>
--- src/components/VisionMision.vue
+++ src/components/VisionMision.vue
@@ -1,68 +1,88 @@
 <template>
-  <div class="container mt-0">
-        <div class="row d-flex align-items-center justify-content-center">
-            <div class="col-md-8 mb-5">
-                <h2 class="card-title"><i><b>Visi</b></i></h2>
-                <h4 class="card-text">Menjadi Perusahaan Energi dan Utilitas terintegrasi kelas dunia untuk menunjang bisnis utama PT Pupuk Indonesia (Persero) beserta anak perusahaannya.</h4>    
-            </div>
-            <div class="col-md-4">
-                <img src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p2/11/2023/09/30/6B977B2A-D585-4BB6-A126-B5D8A8E355BE-4088439324.jpeg" class="img-fluid img-fixed">
-            </div>
+  <div class="vision-mision">
+    <div class="container mt-0" style="margin-bottom: -25px">
+      <div class="row d-flex align-items-center justify-content-center">
+        <div class="col-md-8 mb-5">
+          <h2 class="card-title colored-text-white">
+            <i><b>Visi</b></i>
+          </h2>
+          <h4 class="card-tex colored-text-white">
+            Menjadi Perusahaan Energi dan Utilitas terintegrasi kelas dunia
+            untuk menunjang bisnis utama PT Pupuk Indonesia (Persero) beserta
+            anak perusahaannya.
+          </h4>
         </div>
-
-        <div class="col-md-7 pl-mb-5 order-md-3 colored-text-white">
-          <div class="mb-4" style="text-align: center">
-            <h2>
-              <i><b>Visi</b></i>
-            </h2>
-            <h4 class="text-justify">
-              Menjadi Perusahaan Energi dan Utilitas terintegrasi kelas dunia
-              untuk menunjang bisnis utama PT Pupuk Indonesia (Persero) beserta
-              anak perusahaannya.
-            </h4>
-          </div>
+        <div class="col-md-4">
+          <img
+            src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p2/11/2023/09/30/6B977B2A-D585-4BB6-A126-B5D8A8E355BE-4088439324.jpeg"
+            class="img-fluid img-fixed"
+          />
         </div>
+      </div>
 
-        <div class="col-md-7 pl-mb-5 order-md-3">
-          <div class="mb-4" style="text-align: center">
-            <h2> 
-              <i class="colored-text-white"><b>Misi</b></i>
-            </h2>
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <img class="rounded-image" src="../assets/Plugin icon - 1.png">
-                <br>
-                <br>
-                <p class="colored-text-white">
-                  Melakukan usaha dalam bidang penyediaan energi dan utilitas.
-                </p>
-              </div>
-              <div class="col-md-6 mb-3">
-                <img class="rounded-image" src="../assets/Plugin icon - 1.png">
-                <br>
-                <br>
-                <p class="colored-text-white">Memberikan nilai tambah bagi stakeholders.</p>
-              </div>
-              <div class="col-md-6 mb-3">
-                <img class="rounded-image" src="../assets/Plugin icon - 1.png">
-                <br>
-                <br>
-                <p>
-                  Mengutamakan keselamatan dan kesehatan kerja serta menjaga
-                  pelestarian lingkungan hidup dalam setiap kegiatan
-                  operasional.
-                </p>
-              </div>
-              <div class="col-md-6 mb-3">
-                <img class="rounded-image" src="../assets/Plugin icon - 1.png">
-                <br>
-                <br>
-                <p>Memberikan kontribusi terhadap ketahanan energi nasional.</p>
-              </div>
+      <!-- <div class="col-md-7 pl-mb-5 order-md-3 colored-text-white">
+        <div class="mb-4" style="text-align: center">
+          <h2>
+            <i><b>Visi</b></i>
+          </h2>
+          <h4 class="text-justify">
+            Menjadi Perusahaan Energi dan Utilitas terintegrasi kelas dunia
+            untuk menunjang bisnis utama PT Pupuk Indonesia (Persero) beserta
+            anak perusahaannya.
+          </h4>
+        </div>
+      </div> -->
+
+      <!-- <div class="col-md-7 pl-mb-5 order-md-3"> -->
+      <div
+        class="col-md-7 pl-mb-5 order-md-3"
+      >
+        <div
+          class="mb-4"
+          style="text-align: center; margin: auto;"
+        >
+          <h2 class="mb-3">
+            <i class="colored-text-white"><b>Misi</b></i>
+          </h2>
+          <div class="row">
+            <div class="col-md-6 mb-3">
+              <img class="rounded-image" src="../assets/Plugin icon - 1.png" />
+              <br />
+              <br />
+              <p class="colored-text-white">
+                Melakukan usaha dalam bidang penyediaan energi dan utilitas.
+              </p>
+            </div>
+            <div class="col-md-6 mb-3">
+              <img class="rounded-image" src="../assets/Plugin icon - 1.png" />
+              <br />
+              <br />
+              <p class="colored-text-white">
+                Memberikan nilai tambah bagi stakeholders.
+              </p>
+            </div>
+            <div class="col-md-6 mb-3">
+              <img class="rounded-image" src="../assets/Plugin icon - 1.png" />
+              <br />
+              <br />
+              <p class="colored-text-white">
+                Mengutamakan keselamatan dan kesehatan kerja serta menjaga
+                pelestarian lingkungan hidup dalam setiap kegiatan operasional.
+              </p>
+            </div>
+            <div class="col-md-6 mb-3">
+              <img class="rounded-image" src="../assets/Plugin icon - 1.png" />
+              <br />
+              <br />
+              <p class="colored-text-white">
+                Memberikan kontribusi terhadap ketahanan energi nasional.
+              </p>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -70,22 +90,22 @@ export default {
   name: "VisionMision",
 };
 </script>
-<style>
-  .img-fixed {
-    width: 100%; /* Adjust as needed */
-    height: 250px; /* Adjust as needed */
-    object-fit: cover;
-  }
+<style scoped>
+.img-fixed {
+  width: 100%; /* Adjust as needed */
+  height: 250px; /* Adjust as needed */
+  object-fit: cover;
+}
 .vision-mision {
-    /* background: linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(8, 78, 139, 0.75) 10%, rgba(8, 78, 139, 1) 100%); */
-    background-color: #084e8b;
+  /* background: linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(8, 78, 139, 0.75) 10%, rgba(8, 78, 139, 1) 100%); */
+  background-color: #084e8b;
 }
 .rounded-image {
   border-radius: 25px; /* Adjust the border-radius to control the roundness of the corners */
   overflow: hidden;
 }
 .colored-text-white {
-    color: white;
+  color: white;
 }
 .colored-text-orange {
   /* color: #f15a24; */
