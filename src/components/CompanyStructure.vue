b247e2d960e2bb601aac096c511a50538960b967
I Putu Bagus Adhi Pradana <nyaaitem@gmail.com>
2024-02-12 16:10:51 +0700

    Structure

--- src/components/CompanyStructure.vue
+++ src/components/CompanyStructure.vue
@@ -1,8 +1,21 @@
 <template>
-    <div class="container mt-5">
+    <div class="container mt-5 mb-5">
         <h2 class="text-center" style="color: #084e8b; font-weight: 700; font-style: italic;">Struktur Perusahaan</h2>
-        <img
+        <hr><br>
+        <!-- <img
             src="../assets/strukturorganiasinew.jpg"
+            class="img-fluid zoomable-image d-block mx-auto"
+            alt=""
+        />
+        <br/> -->
+        <!-- <img
+            src="../assets/Struktur/TEST_1.png"
+            class="img-fluid zoomable-image d-block mx-auto"
+            alt=""
+        />
+        <br/> -->
+        <img
+            src="../assets/Struktur/TEST_2.png"
             class="img-fluid zoomable-image d-block mx-auto"
             alt=""
         />
@@ -16,6 +29,9 @@
 </script>
 
 <style scoped>
+    .container{
+        height: 80vh;
+    }
     .zoomable-image {
         transition: transform 0.3s, box-shadow 0.3s;
     }
