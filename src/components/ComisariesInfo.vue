bddbd5002bfc793208a93b04f523fa1ab194f203
I Putu Bagus Adhi Pradana <nyaaitem@gmail.com>
2024-02-22 09:57:07 +0700

    Update Commisare and Director

--- src/components/ComisariesInfo.vue
+++ src/components/ComisariesInfo.vue
@@ -53,10 +53,31 @@
                     
                   </div>
                   <div class="col-md-8 col-12 order-2 order-md-1">
-                    <h2 class="modal-title mb-1">Pengalaman</h2>
-                    <p>
-                      Lahir di Bima, 23 Desember 1966. Menjabat sebagai Komisaris PT Pupuk Indonesai Utilitas sejak 27 Desember 2021. Saat ini juga menjabat sebagai Staf Khusus Menteri Bidang Kebijakan Pertanian, Kementerian Pertanian RI (2019 – Sekarang) dan Staf Pengajar Fakultas Pertanian & Pasca Sarjana Unhas, Makassar (1992 – Sekarang). Sebelumnya menjabat sebagai Direktur Publication Manajement Center Unhas, Makasar (2015-2018), Ketua Dewan Riset Daerah Pemprov Sulawesi Selatan, Makasar (2011-2018), Ketua Tim Ahli Gubernur Pemerintah Propinsi Sulawesi Selatan, Makasar (2009-2018), Tenaga Ahli Bappeda; Dinas Perkebunan; Dinas Pertanian; Tanaman Pangan dan Hirtikultura; Badan Ketahana Pangan Propinsi Sulawesi Selatan (2009-2018), Ketua Badan Perusda Pemprov Sulawesi Selatan (2014-2018), Komisaris GMTDC, Tbk (2009-2014). Meraih gelar Sarjana Sosial Ekonomi Pertanian Universitas Hasanudin (1990), gelar Master of Trade and Development, Newcastle University, Australia (2001), gelar Doktor Ekologi Manusia Institut Pertanian Bogor (2011). 
-                    </p>
+                    <h2 class="modal-title mb-1">Biodata</h2>
+                    <ul>
+                      <li>Nama : Imam Mujahidin Fahmid</li>
+                      <li>Tempat, Tanggal Lahir : Bima, 23 Desember 1966</li>
+                      <li>Pendidikan : 
+                        <ul>
+                          <li>Sarjana Sosial Ekonomi Pertanian Universitas Hasanudin (1990)</li>
+                          <li>Master of Trade and Development Newcastle University Australia (2001)</li> 
+                          <li>Doktor Ekologi Manusia Institut Pertanian Bogor (2011)</li>
+                        </ul>
+                      </li>
+                      <li>Pengalaman : 
+                        <ul>
+                          <li>Menjabat sebagai Komisaris PT Pupuk Indonesai Utilitas (27 Desember 2021 - Sekarang)</li>
+                          <li>Staf Khusus Menteri Bidang Kebijakan Pertanian, Kementerian Pertanian RI (2019 – Sekarang)</li>
+                          <li>Staf Pengajar Fakultas Pertanian & Pasca Sarjana Unhas, Makassar (1992 – Sekarang)</li>
+                          <li>Direktur Publication Manajement Center Unhas, Makasar (2015-2018)</li>
+                          <li>Ketua Dewan Riset Daerah Pemprov Sulawesi Selatan, Makasar (2011-2018)</li>
+                          <li>Ketua Tim Ahli Gubernur Pemerintah Propinsi Sulawesi Selatan, Makasar (2009-2018)</li>
+                          <li>Tenaga Ahli Bappeda; Dinas Perkebunan; Dinas Pertanian; Tanaman Pangan dan Hirtikultura; Badan Ketahana Pangan Propinsi Sulawesi Selatan (2009-2018)</li>
+                          <li>Ketua Badan Perusda Pemprov Sulawesi Selatan (2014-2018)</li>
+                          <li>Komisaris GMTDC, Tbk (2009-2014)</li>
+                        </ul>
+                      </li>
+                    </ul>
                     <div class="d-flex align-items-end">
                       <button
                         type="button"
@@ -124,10 +145,33 @@
                     
                   </div>
                   <div class="col-md-8 col-12 order-2 order-md-1">
-                    <h2 class="modal-title mb-1">Pengalaman</h2>
-                    <p>
-                      Sumyana Sukandar, SE, MM lahir di Bandung, Jawa Barat pada 11 September 1964. Meraih gelar Sarjana Ekonomi jurusan Manajemen dari Universitas Pasundan Bandung pada 1988, kemudian menerima gelar Magister Manajemen dari STIE IPWIJA Jakarta pada 2002. Saat kuliah, Sumyana sudah bekerja sebagai staf tenaga survey BKKBN Provinsi Jawa Barat, kemudian menjadi market researcher di PT Tobby Meza.Co, berlanjut sebagai staf BRI pada 1990-1991. Selanjutnya, Sumyana bergabung di Departemen Keuangan sebagai sebagai pegawai Direktorat Persero, Ditjen Moneter hingga 1995. Beliau kemudian dipercaya menjadi Kepala Seksi Ditjen Pembinaan BUMN, Departemen Keuangan hingga 1998 kemudian menjabat Kepala Subbagian Administrasi pada Kementerian Negara P. BUMN hingga 2000. Kemudian Direktorat Jenderal Pembinaan BUMN, Departemen Keuangan pada 2001-2002. Setelah itu, Sumyana mengabdikan diri di Kementerian BUMN hingga 2015, sempat menjadi Kepala Bidang Usaha Jasa Perencanaan Konstruksi, dan Rekayasa II pada Deputi Bidang Usaha Jasa Lainnya, hingga Asisten Deputi Usaha Perkebunan dan Kehutanan, pada Kedeputian Bidang Usaha Industri Agro dan Industri Strategis. Tahun 2015, Sumyana diangkat menjadi Direktur Administrasi dan Keuangan Perum Jasa Tirta II, hingga menjadi Direktur I Perum Jasa Tirta II pada 2016-2018. Kini, menjabat sebagai Komisaris Utama PT Pupuk Indonesia Energi. 
-                    </p>
+                    <h2 class="modal-title mb-1">Biodata</h2>
+                    <ul>
+                      <li>Nama : Sumyana Sukandar</li>
+                      <li>Tempat, Tanggal Lahir : Bandung, 11 September 1964</li>
+                      <li>Pendidikan : 
+                        <ul>
+                          <li>Sarjana Ekonomi jurusan Manajemen dari Universitas Pasundan Bandung (1988)</li>
+                          <li>Magister Manajemen dari STIE IPWIJA Jakarta (2002)</li>
+                        </ul>
+                      </li>
+                      <li>Pengalaman : 
+                        <ul>
+                          <li>Menjabat sebagai Komisaris Utama PT Pupuk Indonesia Utilitas</li>
+                          <li>Direktur I Perum Jasa Tirta II pada 2016-2018</li>
+                          <li>Direktur Administrasi dan Keuangan Perum Jasa Tirta II pada 2015</li>
+                          <li>Asisten Deputi Usaha Perkebunan dan Kehutanan, pada Kedeputian Bidang Usaha Industri Agro dan Industri Strategis</li>
+                          <li>Kepala Bidang Usaha Jasa Perencanaan Konstruksi, dan Rekayasa II pada Deputi Bidang Usaha Jasa Lainnya</li>
+                          <li>Direktorat Jenderal Pembinaan BUMN, Departemen Keuangan pada 2001-2002</li>
+                          <li>Kepala Subbagian Administrasi pada Kementerian Negara P. BUMN hingga 2000</li>
+                          <li>Kepala Seksi Ditjen Pembinaan BUMN, Departemen Keuangan hingga 1998</li>
+                          <li>Pegawai Direktorat Persero, Departemen Keuangan Ditjen Moneter hingga 1995</li>
+                          <li>Staf BRI (1990-1991)</li>
+                          <li>Market researcher di PT Tobby Meza.Co</li>
+                          <li>Staf tenaga survey BKKBN Provinsi Jawa Barat</li>
+                        </ul>
+                      </li>
+                    </ul>
                     <div class="d-flex align-items-end">
                       <button
                         type="button"
@@ -192,10 +236,27 @@
               <div class="modal-body ">
                 <div class="row">
                   <div class="col-md-8 col-12 order-2 order-md-1">
-                    <h2 class="modal-title mb-1">Pengalaman</h2>
-                    <p>
-                      Lahir di Surabaya, 29 Agustus 1964. Menjabat sebagai Komisaris PT Pupuk Indonesia Utilitas sejak 27 Desember 2021. Sebelumnya menjabat sebagai GM Marketing dan Corporate Communication ANTV, Assisten GM Production ANTV, Supporting Production Manager ANTV dan Sport Manager ANTV. 
-                    </p>
+                    <h2 class="modal-title mb-1">Biodata</h2>
+                    <ul>
+                      <li>Nama : Monica Desideria</li>
+                      <li>Tempat, Tanggal Lahir : Surabaya, 29 Agustus 1964</li>
+                      <li>Pendidikan : 
+                        <ul>
+                          <li>Sarjana Sosial Ekonomi Pertanian Universitas Hasanudin (1990)</li>
+                          <li>Master of Trade and Development Newcastle University Australia (2001)</li> 
+                          <li>Doktor Ekologi Manusia Institut Pertanian Bogor (2011)</li>
+                        </ul>
+                      </li>
+                      <li>Pengalaman : 
+                        <ul>
+                          <li>Menjabat sebagai Komisaris PT Pupuk Indonesia Utilitas (27 Desember 2021 - Sekarang)</li>
+                          <li>GM Marketing dan Corporate Communication ANTV</li>
+                          <li>Assisten GM Production ANTV</li>
+                          <li>Supporting Production Manager ANTV</li>
+                          <li>Sport Manager ANTV</li>
+                        </ul>
+                      </li>
+                    </ul>
                     <div class="d-flex column-flex align-items-end">
                       <button
                         type="button"
